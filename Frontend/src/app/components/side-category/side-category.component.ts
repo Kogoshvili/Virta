e8dca9e7efa8ddb7ac83d919b69487a79f1c3dd7
Andrian Kogoshvili <andriankogoshvili@gmail.com>
2022-08-14 07:11:31 +0400

    fixed side category menu

--- Frontend/src/app/components/side-category/side-category.component.ts
+++ Frontend/src/app/components/side-category/side-category.component.ts
@@ -11,6 +11,85 @@ import { AppStore } from '../../store/app.store';
 })
 export class SideCategoryComponent implements OnInit {
     isSideCategory$ = this.store.select(selectIsSideCategory);
+    categories = [
+        {
+            name: 'Fresh Vegetables',
+            icon: 'flaticon-vegetable',
+            active: false,
+            children: [
+                { name: 'carrot' },
+                { name: 'broccoli' },
+                { name: 'cauliflower' },
+                { name: 'cucumber' },
+            ]
+        },
+        {
+            name: 'Meats',
+            icon: 'flaticon-steak',
+            active: false,
+            children: [
+                { name: 'carrot' },
+                { name: 'broccoli' },
+                { name: 'cauliflower' },
+                { name: 'cucumber' },
+            ]
+        },
+        {
+            name: 'Dairy & Eggs',
+            icon: 'flaticon-farm',
+            active: false,
+            children: [
+                { name: 'carrot' },
+                { name: 'broccoli' },
+                { name: 'cauliflower' },
+                { name: 'cucumber' },
+            ]
+        },
+        {
+            name: 'Sea foods & Fishes',
+            icon: 'flaticon-crab',
+            active: false,
+            children: [
+                { name: 'carrot' },
+                { name: 'broccoli' },
+                { name: 'cauliflower' },
+                { name: 'cucumber' },
+            ]
+        },
+        {
+            name: 'Diet Foods',
+            icon: 'flaticon-salad',
+            active: false,
+            children: [
+                { name: 'carrot' },
+                { name: 'broccoli' },
+                { name: 'cauliflower' },
+                { name: 'cucumber' },
+            ]
+        },
+        {
+            name: 'Dry Foods',
+            icon: 'flaticon-dried-fruits',
+            active: false,
+            children: [
+                { name: 'carrot' },
+                { name: 'broccoli' },
+                { name: 'cauliflower' },
+                { name: 'cucumber' },
+            ]
+        },
+        {
+            name: 'Drinks',
+            icon: 'flaticon-drinks',
+            active: false,
+            children: [
+                { name: 'carrot' },
+                { name: 'broccoli' },
+                { name: 'cauliflower' },
+                { name: 'cucumber' },
+            ]
+        }
+    ];
 
     constructor(
         private store: Store<AppStore>
@@ -22,5 +101,13 @@ export class SideCategoryComponent implements OnInit {
 
     toggleSideCategory(): void {
         this.store.dispatch(toggleIsSideCategory());
+    }
+
+    expandCategory(name: string): void {
+        this.categories.forEach(category => {
+            if (category.name === name) {
+                category.active = !category.active;
+            }
+        });
     }
 }
