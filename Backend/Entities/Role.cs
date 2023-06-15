2ab8e9d4b85403dd32b78f18e0f42a4e482f0f9e
Andrian <kogoshvili@pm.me>
2023-06-16 02:23:46 +0400

    removed warnings

--- Backend/Entities/Role.cs
+++ Backend/Entities/Role.cs
@@ -1,11 +1,9 @@
 using System;
-using System.Collections.Generic;
 using Microsoft.AspNetCore.Identity;
 
 namespace Virta.Entities
 {
     public class Role : IdentityRole<Guid>
     {
-        public virtual ICollection<UserRole> UserRoles { get; set; }
     }
 }
--- Backend/Entities/UserRole.cs
+++ Backend/Entities/UserRole.cs
@@ -5,7 +5,5 @@ namespace Virta.Entities
 {
     public class UserRole : IdentityUserRole<Guid>
     {
-        public virtual User User { get; set; }
-        public virtual Role Role { get; set; }
     }
 }
