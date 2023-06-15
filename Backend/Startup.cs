ab392d11c157369a7b2bd44e84846dabd84ec03d
Andrian <kogoshvili@pm.me>
2023-06-16 03:03:57 +0400

    renaming

--- Backend/Startup.cs
+++ Backend/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
-using Virta.Data;
+using Virta.Database;
 using Virta.Extensions;
 
 namespace Virta
